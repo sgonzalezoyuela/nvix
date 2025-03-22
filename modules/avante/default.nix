347ac678a95a520b03cf9adb63f1ee42d525327e
sgonzalez <sgonzalezoyuela@gmail.com>
2025-03-22 19:34:44 -0300

    improved avant config: in particular start ask/edit windows in normal mode to avoid input issues

--- modules/avante/default.nix
+++ modules/avante/default.nix
@@ -1,23 +1,45 @@
 { lib, ... }:
 {
-
   plugins.avante = {
     enable = true;
     autoLoad = true;
-
     luaConfig.post = ''
-        vim.opt.laststatus = 3
-        require("avante").setup({
+      require("avante").setup({
+
         claude = {
-          model = "claude-3-7-sonnet-20250219",  -- Updated model value
+          model = "claude-3-7-sonnet-20250219"  -- Updated model value
         },
+
         windows = {
-            position = "right"
+          ---@alias AvantePosition "right" | "left" | "top" | "bottom" | "smart"
+          position = "left",
+          wrap = true, -- similar to vim.o.wrap
+          width = 30, -- default % based on available width in vertical layout
+          height = 30, -- default % based on available height in horizontal layout
+          sidebar_header = {
+            enabled = true, -- true, false to enable/disable the header
+            align = "center", -- left, center, right for title
+            rounded = true,
+          },
+          input = {
+            prefix = "> ",
+            height = 8, -- Height of the input window in vertical layout
+          },
+          edit = {
+            border = "rounded",
+            start_insert = false, -- Start insert mode when opening the edit window
+          },
+          ask = {
+            floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+            border = "rounded",
+            start_insert = false, -- Start insert mode when opening the ask window
+            ---@alias AvanteInitialDiff "ours" | "theirs"
+            focus_on_apply = "ours", -- which diff to focus after applying
+          },
         }
       })
     '';
   };
-
   imports = with builtins; with lib;
     map (fn: ./${fn})
       (filter
