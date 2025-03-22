912ed947c56dcb7303edeab21d6a0a5dd3ca2c21
sgonzalez <sgonzalezoyuela@gmail.com>
2025-03-22 11:22:31 -0300

    Avante plugin config

--- modules/avante/default.nix
+++ modules/avante/default.nix
@@ -4,6 +4,18 @@
   plugins.avante = {
     enable = true;
     autoLoad = true;
+
+    luaConfig.post = ''
+        vim.opt.laststatus = 3
+        require("avante").setup({
+        claude = {
+          model = "claude-3-7-sonnet-20250219",  -- Updated model value
+        },
+        windows = {
+            position = "left"
+        }
+      })
+    '';
   };
 
   imports = with builtins; with lib;
