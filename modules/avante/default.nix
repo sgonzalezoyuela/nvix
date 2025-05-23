85b59890a1eba32b6be3bbd39c6c049d9917c9f9
sgonzalez <sgonzalezoyuela@gmail.com>
2025-05-23 18:16:05 -0300

    Updated nixvim + claude 4

--- modules/avante/default.nix
+++ modules/avante/default.nix
@@ -13,6 +13,10 @@
            model = "gemini-2.5-pro-preview-05-06"
         },
 
+        claude = {
+           model = "claude-4-0-sonnet-2025052"
+        },
+
         windows = {
           ---@alias AvantePosition "right" | "left" | "top" | "bottom" | "smart"
           position = "left",
