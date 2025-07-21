84e6ffaa6f056fe7f0f02ed0ea4bfa67da7f2b34
sgonzalez <sgonzalezoyuela@gmail.com>
2025-07-21 15:09:51 -0300

    Updated flake + merged AI config

--- packages/default.nix
+++ packages/default.nix
@@ -29,8 +29,7 @@ let
 
     # Productivity
     self.nvixPlugins.autosession
-    #self.nvixPlugins.ai
-    self.nvixPlugins.avante
+    self.nvixPlugins.ai
 
     # Dashboard (Auto session works so rarely i see this.)
     self.nvixPlugins.dashboard
